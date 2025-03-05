8d4abd0d9058938ac2c8760987b78b5f45fa6c0f
Neeaj Kumar <neerajkumar08697@gmail.com>
2025-03-05 11:49:32 +0530

    add Navbar component

---
+++ src/components/Shop/Navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+import { ShoppingCart, User } from "lucide-react";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav
+      className="w-full flex  justify-between  
+    px-5 py-4 shadow-md
+    "
+    >
+      <div>
+        <h2 className="font-bold">SnapShop</h2>
+      </div>
+      <div>
+        <ul className="flex gap-3 ">
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">Products</a>
+          </li>
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Contact </a>
+          </li>
+        </ul>
+      </div>
+      <div className="flex items-center gap-2 ">
+        <ShoppingCart size={20} />
+        <User size={20} />
+        <a href="">Login</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
