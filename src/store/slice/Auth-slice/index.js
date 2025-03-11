d9957e657d7f9ba0ba5ee2bb5b6a86a754721771
Neeaj Kumar <neerajkumar08697@gmail.com>
2025-03-11 23:01:54 +0530

    Add registerUser thunk

--- src/store/slice/Auth-slice/index.js
+++ src/store/slice/Auth-slice/index.js
@@ -1,11 +1,41 @@
-import { createSlice } from "@reduxjs/toolkit";
-const initialState = {};
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+const initialState = {
+  isLoading: false,
+  userData: null,
+};
+
+export const registerUser = createAsyncThunk("register", async (formData) => {
+  try {
+    console.log(formData);
+    const response = await axios.post(
+      `${import.meta.env.VITE_BACKEND_URL}/auth/register`,
+      formData
+    ); 
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 const authSlice = createSlice({
   name: "authSlice",
   initialState,
   reducers: {},
-  extraReducers: () => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(registerUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.userData = action.payload;
+      })
+      .addCase(registerUser.rejected, (state, action) => {
+        state.isLoading = true;
+        state.userData = null;
+      });
+  },
 });
 
 export default authSlice.reducer;
