b75b549a6e7e1aad2fb0eafb83d58e43a65d8b8b
Neeaj Kumar <neerajkumar08697@gmail.com>
2025-03-11 22:04:12 +0530

    Add auth reducer to store configuration

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "./slice/product-slice/index";
+import authReducer from "./slice/Auth-slice/index";
 const store = configureStore({
   reducer: {
     product: productReducer,
+    auth: authReducer,
   },
 });
 
