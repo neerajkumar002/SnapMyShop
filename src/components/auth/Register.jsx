098ca6d67eed44d60f9e2859eef5381924c5e677
Neeaj Kumar <neerajkumar08697@gmail.com>
2025-03-11 22:18:40 +0530

    add registration form with validation using react-hook-form

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,10 +1,20 @@
-import { Lock, Mail, User2 } from "lucide-react";
+import {  Lock, Mail, User2 } from "lucide-react";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 const Register = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="py-5">
       <h1 className="text-center font-semibold text-2xl">Create Account</h1>
-      <form className="px-3 flex flex-col gap-5">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="px-3 flex flex-col gap-5"
+      >
         <div className="flex flex-col gap-2">
           <label htmlFor="" className="flex items-center gap-2 text-gray-500">
             <User2 /> Full Name
@@ -12,6 +22,7 @@ const Register = () => {
           <input
             type="text"
             placeholder="Full Name"
+            {...register("fullName", { required: true })}
             className="px-2 py-2 border  rounded-md border-gray-400"
           />
         </div>
@@ -22,6 +33,7 @@ const Register = () => {
           <input
             type="text"
             placeholder="Email"
+            {...register("email", { required: true })}
             className="px-2 py-2 border  rounded-md border-gray-400"
           />
         </div>
@@ -32,11 +44,12 @@ const Register = () => {
           <input
             type="password"
             placeholder="Password"
+            {...register("password", { required: true })}
             className="px-2 py-2 border  rounded-md border-gray-400"
           />
         </div>
         <div className="flex flex-col gap-2">
-          <button className="bg-green-500 text-white py-2 rounded-md font-semibold">
+          <button className="cursor-pointer bg-green-500 text-white py-2 rounded-md font-semibold">
             {" "}
             Sign Up
           </button>
