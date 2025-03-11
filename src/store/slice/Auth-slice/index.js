5e7ae16652053e5cad6be5a62f2f5483af915859
Neeaj Kumar <neerajkumar08697@gmail.com>
2025-03-11 22:03:38 +0530

    Add Auth slice

---
+++ src/store/slice/Auth-slice/index.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {};
+
+const authSlice = createSlice({
+  name: "authSlice",
+  initialState,
+  reducers: {},
+  extraReducers: () => {},
+});
+
+export default authSlice.reducer;
