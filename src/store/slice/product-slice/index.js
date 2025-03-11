4707c98f5b2d61a1675569920082650033eef0d0
Neeaj Kumar <neerajkumar08697@gmail.com>
2025-03-11 12:26:15 +0530

    add async thunk fetch all products and update loading state

--- src/store/slice/product-slice/index.js
+++ src/store/slice/product-slice/index.js
@@ -1,15 +1,39 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
   isLoading: false,
   productList: [],
 };
 
+export const fetchAllProducts = createAsyncThunk("", async () => {
+  try {
+    const response = await axios.get(
+      `${import.meta.env.VITE_BACKEND_URL}/admin/products/get`
+    );
+    return response.data;
+  } catch (error) {
+    console.log("Error: Fetch products", error);
+  }
+});
+
 const productSlice = createSlice({
   name: "productSlice",
   initialState,
   reducers: {},
-  extraReducers: () => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAllProducts.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchAllProducts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.productList = action.payload.data;
+      })
+      .addCase(fetchAllProducts.rejected, (state) => {
+        state.isLoading = true;
+      });
+  },
 });
 
 export default productSlice.reducer;
