5f9fe5553490a75b964bc1df1babed3465122b68
Neeaj Kumar <neerajkumar08697@gmail.com>
2025-03-12 16:09:24 +0530

    add loginUser thunk and update registerUser thunk for improved error handling

--- src/store/slice/Auth-slice/index.js
+++ src/store/slice/Auth-slice/index.js
@@ -5,13 +5,30 @@ const initialState = {
   userData: null,
 };
 
-export const registerUser = createAsyncThunk("register", async (formData) => {
+export const registerUser = createAsyncThunk(
+  "auth/register",
+  async (formData) => {
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_BACKEND_URL}/auth/register`,
+        formData
+      );
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+//login user
+export const loginUser = createAsyncThunk("auth/login", async (formData) => {
   try {
-    console.log(formData);
     const response = await axios.post(
-      `${import.meta.env.VITE_BACKEND_URL}/auth/register`,
-      formData
-    ); 
+      `${import.meta.env.VITE_BACKEND_URL}/auth/login`,
+      formData,
+      { withCredentials: true }
+    );
+    console.log(response.data);
     return response.data;
   } catch (error) {
     console.log(error);
@@ -32,6 +49,17 @@ const authSlice = createSlice({
         state.userData = action.payload;
       })
       .addCase(registerUser.rejected, (state, action) => {
+        state.isLoading = true;
+        state.userData = null;
+      })
+      .addCase(loginUser.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.userData = action.payload;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
         state.isLoading = true;
         state.userData = null;
       });
