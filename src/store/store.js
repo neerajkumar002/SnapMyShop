c4ed031e12d8a0e58de3d9512a6b2230d07f703a
Neeaj Kumar <neerajkumar08697@gmail.com>
2025-04-01 12:31:51 +0530

    feat: add reducers for cart. address, order, and admin orders

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "./slice/product-slice/index";
 import authReducer from "./slice/Auth-slice/index";
+import cartReducer from "./slice/Cart-Slice/index";
+import addressReducer from "./slice/Address-slice/index";
+import orderReducer from "./slice/Order-slice/index";
+import adminorderReducer from "./admin/order-slice/index";
 const store = configureStore({
   reducer: {
     product: productReducer,
     auth: authReducer,
+    cart: cartReducer,
+    address: addressReducer,
+    order: orderReducer,
+    adminOrders: adminorderReducer,
   },
 });
 
