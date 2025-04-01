ba08c8faa29cced5de51637cb3effe5b6c29ef27
Neeaj Kumar <neerajkumar08697@gmail.com>
2025-04-01 23:15:57 +0530

    feat: add loading state and reviews sectioin in ProductDetails component

--- src/Pages/Shop/Products/Detail.jsx
+++ src/Pages/Shop/Products/Detail.jsx
@@ -3,9 +3,11 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getProductDetail } from "../../../store/slice/product-slice";
+import ReviewCard from "../Review/ReviewCard";
+import ProductDetailsShimmer from "../../../components/Shop/Shimmer/ProductDetailsShimmer";
 
 const ProductDetail = () => {
-  const { productDetail } = useSelector((state) => state.product);
+  const { productDetail, isLoading } = useSelector((state) => state.product);
   const dispatch = useDispatch();
   const { id } = useParams();
 
@@ -14,30 +16,32 @@ const ProductDetail = () => {
       dispatch(getProductDetail(id));
     }
   }, [dispatch, id]);
+ 
 
-  console.log(productDetail);
+  if (isLoading) return <ProductDetailsShimmer />;
 
   return (
-    <div className="px-6 ">
+    <div className="lg:px-6 ">
       <div className="w-full  py-6  lg:px-8">Home > Mens </div>
-      <div className="flex flex-col  lg:flex-row gap-7 justify-center">
+
+      <div className="flex flex-col  lg:flex-row gap-7 justify-center lg:px-8">
         <div className="lg:w-[500px]">
           <img src={productDetail?.image || "/public/placeholder.svg"} alt="" />
         </div>
         <div className="flex flex-col   gap-2 lg:w-[800px]">
           <h2 className="font-bold">{productDetail?.title} </h2>
-          <p className="text-sm">
-            {
-              productDetail?.description
-            }
-          </p>
+          <p className="text-sm">{productDetail?.description}</p>
           <div>
             <button className="bg-gray-200 w-full py-2 flex justify-center gap-3 items-center">
               <Truck /> Free Delivery
             </button>
           </div>
-          <p className="line-through text-gray-400">₹{productDetail?.price*2}</p>
-          <p className="text-xl text-red-600 font-semibold">₹{productDetail?.price}</p>
+          <p className="line-through text-gray-400">
+            ₹{productDetail?.price * 2}
+          </p>
+          <p className="text-xl text-red-600 font-semibold">
+            ₹{productDetail?.price}
+          </p>
           <div className="flex gap-2">
             <Star fill="gold" stroke="none" />
             <Star fill="gold" stroke="none" />
@@ -59,6 +63,18 @@ const ProductDetail = () => {
             <button className="border border-gray-300 py-2 px-2">
               <Heart />
             </button>
+          </div>
+          <div className="mb-3">
+            <h3 className="font-semibold text-xl py-2">Reviews</h3>
+            <div className="flex flex-col gap-2 min-h-[100px] max-h-[240px] overflow-y-auto">
+              <ReviewCard />
+              <ReviewCard />
+              <ReviewCard />
+              <ReviewCard />
+              <ReviewCard />
+              <ReviewCard />
+              <ReviewCard />
+            </div>
           </div>
         </div>
       </div>
