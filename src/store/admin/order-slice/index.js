3524db23bdc79169b80ae2448371172b268dbfd2
Neeaj Kumar <neerajkumar08697@gmail.com>
2025-04-01 12:33:32 +0530

    feat: implement async thunks for fetching admin orders and order details

--- src/store/admin/order-slice/index.js
+++ src/store/admin/order-slice/index.js
@@ -1,4 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
   isLoading: false,
@@ -6,11 +7,67 @@ const initialState = {
   orderDetails: null,
 };
 
+export const fetchAllOrdersForAdmin = createAsyncThunk(
+  "/order/fetchAllOrdersForAdmin",
+  async () => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_BASE_API_URL}/admin/orders/list`
+      );
+
+      console.log("orders", response.data);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+export const fetchOrderDetailsForAdmin = createAsyncThunk(
+  "/order/fetchOrderDetailsForAdmin",
+  async (id) => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_BASE_API_URL}/admin/orders/details/${id}`
+      );
+
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
 const adminOrdersSlice = createSlice({
   name: "adminOrdersSlice",
   initialState,
   reducers: {},
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAllOrdersForAdmin.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchAllOrdersForAdmin.fulfilled, (state, action) => {
+        state.isLoading = false;
+
+        state.ordersList = action.payload.data;
+      })
+      .addCase(fetchAllOrdersForAdmin.rejected, (state) => {
+        state.isLoading = true;
+        state.ordersList = [];
+      })
+      .addCase(fetchOrderDetailsForAdmin.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchOrderDetailsForAdmin.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.orderDetails = action.payload.data;
+      })
+      .addCase(fetchOrderDetailsForAdmin.rejected, (state) => {
+        state.isLoading = true;
+        state.orderDetails = [];
+      });
+  },
 });
 
 export default adminOrdersSlice.reducer;
