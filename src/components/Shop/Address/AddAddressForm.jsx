b4b17cf779348b451903dd01718cc4bcef44e506
Neeaj Kumar <neerajkumar08697@gmail.com>
2025-04-02 16:29:37 +0530

    feat: implement address form with validation and address limit

--- src/components/Shop/Address/AddAddressForm.jsx
+++ src/components/Shop/Address/AddAddressForm.jsx
@@ -1,7 +1,5 @@
-import { 
+import {
   Building2Icon,
-  Earth,
-  Home,
   Mail,
   MapPin,
   MapPinCheckInside,
@@ -9,15 +7,56 @@ import {
   Phone,
   User2,
 } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addAddress,
+  fetchAllAddresses,
+} from "../../../store/slice/Address-slice";
+import { toast } from "react-toastify";
+import { useEffect } from "react";
 const AddAddressForm = () => {
+  const { userData } = useSelector((state) => state.auth);
+  const { addressList } = useSelector((state) => state.address);
+  const dispatch = useDispatch();
+  const toastify = (message) => toast(message);
+
+  const { register, handleSubmit, reset } = useForm();
+
+  const onSubmit = (data) => {
+    if (addressList.length >= 3) {
+      toastify("You can add max 3 addresses");
+      reset();
+    } else {
+      const addressData = { ...data, userId: userData?.id };
+      dispatch(addAddress(addressData)).then((data) => {
+        console.log(data);
+        if (data?.payload?.success) {
+          toastify(data?.payload?.message);
+          reset();
+        }
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (userData) {
+      dispatch(fetchAllAddresses(userData?.id));
+    }
+  }, [dispatch, userData]);
+
   return (
-    <form className="px-4 flex flex-col gap-3">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="px-4 flex flex-col gap-3"
+    >
       <div className=" flex flex-col gap-3">
         <label htmlFor="" className="flex gap-2 text-gray-400">
           <User2 /> Full Name
         </label>
         <input
           type="text"
+          {...register("fullName", { required: true })}
           placeholder="Full Name (Required)*"
           className="px-1 py-3 border outline-none rounded-md"
         />
@@ -28,8 +67,9 @@ const AddAddressForm = () => {
         </label>
         <input
           type="text"
+          {...register("email", { required: true })}
           placeholder="Email ID (Required)*"
-        className="px-1 py-3 border outline-none rounded-md"
+          className="px-1 py-3 border outline-none rounded-md"
         />
       </div>
       <div className=" flex flex-col gap-3">
@@ -38,28 +78,21 @@ const AddAddressForm = () => {
         </label>
         <input
           type="text"
+          {...register("phone", { required: true })}
           placeholder="Phone Number (Required)*"
-      className="px-1 py-3 border outline-none rounded-md"
-        />
-      </div>
-      <div className=" flex flex-col gap-3">
-        <label htmlFor="" className="flex gap-2 text-gray-400">
-          <Home /> House No., Building Name
-        </label>
-        <input
-          type="text"
-          placeholder=" House No., Building Name (Required)*"
           className="px-1 py-3 border outline-none rounded-md"
         />
       </div>
+
       <div className=" flex flex-col gap-3">
         <label htmlFor="" className="flex gap-2 text-gray-400">
-          <MapPin /> Road Name, Area Colony
+          <MapPin /> Address (House No, Building, Street, Area)*
         </label>
         <input
           type="text"
-          placeholder="Road Name, Area Colony (Required)*"
-        className="px-1 py-3 border outline-none rounded-md"
+          {...register("address", { required: true })}
+          placeholder="Address (House No, Building, Street, Area)*"
+          className="px-1 py-3 border outline-none rounded-md"
         />
       </div>
       <div className=" flex flex-col gap-3">
@@ -68,8 +101,9 @@ const AddAddressForm = () => {
         </label>
         <input
           type="text"
+          {...register("city", { required: true })}
           placeholder="City (Required)*"
-       className="px-1 py-3 border outline-none rounded-md"
+          className="px-1 py-3 border outline-none rounded-md"
         />
       </div>
       <div className=" flex flex-col gap-3">
@@ -78,34 +112,27 @@ const AddAddressForm = () => {
         </label>
         <input
           type="text"
+          {...register("state", { required: true })}
           placeholder="State (Required)*"
           className="px-1 py-3 border outline-none rounded-md"
         />
       </div>
-      <div className=" flex flex-col gap-3">
-        <label htmlFor="" className="flex gap-2 text-gray-400">
-          <Earth /> Country
-        </label>
-        <input
-          type="text"
-          placeholder="Country (Required)*"
-          className="px-1 py-3 border outline-none rounded-md"
-        />
-      </div>
+
       <div className=" flex flex-col gap-3">
         <label htmlFor="" className="flex gap-2 text-gray-400">
           <MapPinCheckInside />
           Pincode
         </label>
         <input
-          type="number"
+          type="text"
+          {...register("pincode", { required: true })}
           placeholder="Pincode (Required)*"
           className="px-1 py-3 border outline-none rounded-md"
         />
       </div>
-      <div className="py-5" >
+      <div className="py-5">
         <button className="bg-green-600 w-full py-3 rounded-md font-semibold text-white">
-            Save Address
+          Save Address
         </button>
       </div>
     </form>
