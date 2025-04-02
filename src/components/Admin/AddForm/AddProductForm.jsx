f72913def186e2c7f6f3ec71bb05a0a359500617
Neeaj Kumar <neerajkumar08697@gmail.com>
2025-04-02 16:15:51 +0530

    feat: implement form handling  for adding new products with validation

--- src/components/Admin/AddForm/AddProductForm.jsx
+++ src/components/Admin/AddForm/AddProductForm.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { addNewProduct } from "../../../store/admin/product-slice";
 
 const AddProductForm = () => {
   const [selectedImage, setSelectedImage] = useState(null);
+  const dispatch = useDispatch();
   console.log(selectedImage);
 
   function handleImageChange(e) {
@@ -9,13 +13,34 @@ const AddProductForm = () => {
     if (file) setSelectedImage(URL.createObjectURL(file));
   }
 
+  const { register, handleSubmit } = useForm();
+
+  const handleAddProductForm =async (data) => {
+    const formData = new FormData();
+    formData.append("title", data.title);
+    formData.append("description", data.description);
+    formData.append("price", data.price);
+    formData.append("category", data.category);
+    if (data.file && data.file.length > 0) {
+      formData.append("image", data.file[0]);
+    }
+
+    for (let x of formData.entries()) {
+      console.log(x);
+    }
+    dispatch(addNewProduct(formData));
+  };
+
   return (
     <div className="  ">
       <div className="px-3 py-2 ">
         <h3 className="font-semibold text-xl">Add New Product</h3>
       </div>
       <div>
-        <form action="" className="  flex flex-col gap-3 px-3">
+        <form
+          onSubmit={handleSubmit(handleAddProductForm)}
+          className="  flex flex-col gap-3 px-3"
+        >
           <div>
             <div className="  w-[100px] h-[100px] cursor-pointer">
               <label htmlFor="productImage">
@@ -29,6 +54,7 @@ const AddProductForm = () => {
             <input
               type="file"
               id="productImage"
+              {...register("file", { required: true })}
               onChange={(e) => handleImageChange(e)}
               className="hidden"
             />
@@ -41,6 +67,7 @@ const AddProductForm = () => {
               type="text"
               id="title"
               placeholder="Product Title"
+              {...register("title", { required: true })}
               className="border rounded-md px-2 py-2 outline-blue-300/40"
             />
           </div>
@@ -52,18 +79,20 @@ const AddProductForm = () => {
               type="text"
               id="description"
               placeholder="Product Description"
+              {...register("description", { required: true })}
               className="border rounded-md px-2 py-2 outline-blue-300/40"
             />
           </div>
           <div className="flex justify-between gap-2 lg:w-[400px]">
             <div className="flex flex-col gap-2">
-              <label htmlFor="categroy" className="">
-                Product Categroy
+              <label htmlFor="category" className="">
+                Product category
               </label>
               <select
-                name="categroy"
-                id="categroy"
-                className="border rounded-md py-2 w-[150px] lg:w-[200px]"
+                name="category"
+                id="category"
+                {...register("category", { required: true })}
+                className="border rounded-md py-2  w-[150px] lg:w-[200px]"
               >
                 <option value="mens-clothing">Men</option>
                 <option value="womens-clothing">Women</option>
@@ -77,6 +106,7 @@ const AddProductForm = () => {
                 type="number"
                 id="price"
                 placeholder="100"
+                {...register("price", { required: true })}
                 className="border w-[150px] rounded-md px-2 py-2 outline-blue-300/40"
               />
             </div>
