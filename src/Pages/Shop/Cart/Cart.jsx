841481830305d380f5aa66ffc7d8b8c811cf5581
Neeaj Kumar <neerajkumar08697@gmail.com>
2025-04-02 16:51:30 +0530

    feat: implement dynamic cart display and total calculation in Cart Component

--- src/Pages/Shop/Cart/Cart.jsx
+++ src/Pages/Shop/Cart/Cart.jsx
@@ -1,35 +1,100 @@
+import { useDispatch, useSelector } from "react-redux";
 import CartItem from "../../../components/Shop/Cart/CartItem";
+import { useEffect } from "react";
+import { getCart } from "../../../store/slice/Cart-Slice";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
+  const { userData } = useSelector((state) => state.auth);
+  const { cart, isLoading } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (userData) {
+      dispatch(getCart(userData?.id));
+    }
+  }, [dispatch, userData]);
+
+  const cartTotal =
+    cart && cart?.items?.length > 0
+      ? cart.items.reduce(
+          (sum, currentItem) => sum + currentItem.price * currentItem.quantity,
+          0
+        )
+      : 0;
+
   return (
     <div className="py-5">
-      <div>
-        <h1 className="font-bold text-3xl text-center">Your Shopping Cart</h1>
-      </div>
-      <div className="flex flex-col items-center py-10 gap-3  px-5 ">
-        <CartItem />
-        <CartItem />
-        <CartItem />
-      </div>
-      <div className="flex justify-end pr-6 lg:pr-92  ">
-        <div className="text-right flex flex-col gap-1">
-          <p className="text-gray-500">
-            SubTotal: <span className="text-green-500 font-semibold">₹280.11</span>
-          </p>
-          <p className="text-gray-500">
-            Shipping: <span className="text-green-500 font-semibold">Free</span>
-          </p>
-          <p className="text-gray-500">
-            Taxes: <span className="text-green-500 font-semibold">₹10.00</span>
-          </p>
-          <p className="text-gray-500  text-2xl">
-            Total: <span className="text-red-400 font-bold">₹290.11</span>
-          </p>
-          <div className="pt-4">
-            <button className="bg-green-400 rounded text-white px-5 py-3 font-bold">Confirm Checkout</button>
+      {cart?.length === 0 || !cart ? (
+        <div className="text-center py-10">
+          <h1 className="font-semibold text-2xl text-gray-400">
+            Your cart is empty
+          </h1>
+          <button
+            onClick={() => navigate("/products")}
+            className="bg-red-500 text-white px-3 py-2  rounded-md mt-5 cursor-pointer"
+          >
+            Shop Now
+          </button>
+        </div>
+      ) : (
+        <div>
+          <div>
+            <h1 className="font-bold text-3xl text-center">
+              Your Shopping Cart
+            </h1>
+          </div>
+          <div className="flex flex-col   items-center py-10  px-5 ">
+            <div className=" flex flex-col  gap-3 lg:w-[800px]">
+              {cart && cart?.items?.length > 0
+                ? cart.items.map((item) => (
+                    <CartItem
+                      key={item.productId}
+                      title={item.title}
+                      image={item.image}
+                      quantity={item.quantity}
+                      price={item.price}
+                      item={item}
+                    />
+                  ))
+                : null}{" "}
+            </div>
+          </div>
+          <div className="flex justify-end pr-6 lg:pr-92  ">
+            <div className="text-right flex flex-col gap-1">
+              <p className="text-gray-500">
+                SubTotal:{" "}
+                <span className="text-green-500 font-semibold">
+                  ₹{cartTotal.toFixed(2)}
+                </span>
+              </p>
+              <p className="text-gray-500">
+                Shipping:{" "}
+                <span className="text-green-500 font-semibold">Free</span>
+              </p>
+              <p className="text-gray-500">
+                Taxes:{" "}
+                <span className="text-green-500 font-semibold">₹10.00</span>
+              </p>
+              <p className="text-gray-500  text-2xl">
+                Total:{" "}
+                <span className="text-red-400 font-bold">
+                  ₹{(cartTotal + 10).toFixed(2)}
+                </span>
+              </p>
+              <div className="pt-4">
+                <button
+                  onClick={() => navigate("/checkout")}
+                  className="bg-green-400 rounded text-white px-5 py-3 font-bold"
+                >
+                  Confirm Checkout
+                </button>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
