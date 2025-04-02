389fdac1a55db32336c0f7226355b0bf14226e89
Neeaj Kumar <neerajkumar08697@gmail.com>
2025-04-02 16:53:11 +0530

    feat: add navigation to home from Auth component

--- src/Pages/auth/Auth.jsx
+++ src/Pages/auth/Auth.jsx
@@ -1,12 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Login from "../../components/auth/Login";
 import { MoveLeft } from "lucide-react";
 
 const Auth = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
       <div className="border-b border-gray-400 py-3 px-4">
-        <button className="flex gap-3 text-2xl items-center text-gray-500">
+        <button
+          onClick={() => navigate("/")}
+          className="flex gap-3 text-2xl items-center text-gray-500 cursor-pointer"
+        >
           <MoveLeft /> Back
         </button>
       </div>
