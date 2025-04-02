5df9402a0d26ec44504213612f59c000ec575a58
Neeaj Kumar <neerajkumar08697@gmail.com>
2025-04-02 16:52:11 +0530

    feat: add navigation to checkout page from add address component

--- src/Pages/Shop/Address/AddAddress.jsx
+++ src/Pages/Shop/Address/AddAddress.jsx
@@ -1,14 +1,14 @@
-import {
-  ArrowLeft,
- 
-} from "lucide-react";
+import { ArrowLeft } from "lucide-react";
 import AddAddressForm from "../../../components/Shop/Address/AddAddressForm";
+import { useNavigate } from "react-router-dom";
 
 const AddAddress = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
       <div className="flex py-4 px-3 font-semibold ">
-        <button className="cursor-pointer pr-2">
+        <button onClick={()=>navigate("/checkout")} className="cursor-pointer pr-2">
           <ArrowLeft />
         </button>{" "}
         Add Address
