64c7a7eee9905ab4736298da99499dfec4d359bf
Neeaj Kumar <neerajkumar08697@gmail.com>
2025-04-03 14:45:01 +0530

    enhance AdminOrderItem component with order date and total amount display

--- src/components/Admin/Order/OrderItem.jsx
+++ src/components/Admin/Order/OrderItem.jsx
@@ -1,47 +1,45 @@
-import { ArrowRight } from "lucide-react";
+import { format } from "date-fns";
+import { ArrowRight, ReceiptText } from "lucide-react";
 
 const AdminOrderItem = ({
   id,
   totalAmount,
   totalItems,
-  status,
+  orderStatus,
+  orderDate,
   setShowModal,
   setCurrentOrderId,
 }) => {
+  console.log(orderStatus);
   return (
-    <div className="w-full flex justify-between lg:w-[800px] h-[100px] shadow">
-      <div className=" flex gap-5 items-center px-2 ">
-        {/* <div className="w-[100px]">
-        <img src="/public/placeholder.svg" alt="" className="object-cover" />
-      </div> */}
-        <div className="flex flex-col gap-2">
-          <p className="font-semibold">
-            Order ID: <span>{id}</span>
-          </p>
-          <p>{totalItems} Items</p>
-          {/* <p>
-              {orderDate &&
-                format(new Date(orderDate), "MMMM dd, yyyy, hh:mm aa")}
-            </p> */}
+    <div
+      onClick={() => {
+        setShowModal(true);
+        setCurrentOrderId(id);
+      }}
+      className="w-full flex justify-between  h-[100px] shadow hover:scale-102 transition-transform duration-300 cursor-pointer"
+    >
+      <div className=" flex gap-5 items-center px-2 w-full ">
+        <div className="flex flex-col gap-2 lg:gap-7  ">
+          <div className="flex flex-col lg:flex-row gap-2 lg:gap-10">
+            <p className="font-semibold text-sm text-gray-600">
+              Order ID: <span>{id}</span>
+            </p>
+            <p className="font-semibold text-sm">
+              Total Amount: ₹ {totalAmount}
+            </p>
+          </div>
+          <div className="flex gap-3">
+            <p>{totalItems?.length} Items</p>
+            <p>{format(new Date(orderDate), "MMMM dd, yyyy, hh:mm aa")}</p>
+          </div>
         </div>
       </div>
 
       <div className="pt-3 pr-3">
-        <p className="font-bold">₹{totalAmount}</p>
         <p className="bg-green-500 rounded-full px-2 text-white mt-4">
-          {status}
+          {orderStatus}
         </p>
-      </div>
-      <div className="flex items-center pr-2">
-        <button
-          onClick={() => {
-            setShowModal(true);
-            setCurrentOrderId(id);
-          }}
-          className="text-red-600 cursor-pointer flex gap-2"
-        >
-          Order Details <ArrowRight />
-        </button>
       </div>
     </div>
   );
