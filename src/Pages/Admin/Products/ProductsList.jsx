e08cd3ddeca38b8c8203d2e3e40e418a6d54de29
Neeaj Kumar <neerajkumar08697@gmail.com>
2025-04-03 09:36:46 +0530

    add Edit and delete icons

--- src/Pages/Admin/Products/ProductsList.jsx
+++ src/Pages/Admin/Products/ProductsList.jsx
@@ -1,18 +1,48 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAllProducts } from "../../../store/slice/product-slice";
+import { SquarePen, Trash2Icon } from "lucide-react";
 
 const AdminProductItem = ({ id, image, title, price, category }) => {
   return (
-    <div className=" bg-white shadow-md flex gap-3 rounded-md ">
-      <div className="w-[50px] h-[60px] overflow-hidden ">
-        <img src={image || "/public/placeholder.svg"} alt="" className="w-full h-full "/>
-      </div>
-      <div>
-        <p>{title}</p>
-        <div className="flex items-center gap-3">
-          <p>Price: {price}</p> <p>Category: {category}</p>
+    <div className=" bg-white shadow-md flex justify-between gap-3 rounded-md ">
+      <div className="flex gap-3">
+        <div className="w-[80px]  overflow-hidden ">
+          <img
+            src={image || "/public/placeholder.svg"}
+            alt=""
+            className="w-full h-full "
+          />
         </div>
+        <div>
+          <p className="text-gray-700">{title}</p>
+          <p className="text-gray-600">
+            Product ID: <span className="text-gray-700">{id}</span>{" "}
+          </p>
+          <div className="flex items-center gap-3">
+            <p className="text-gray-600">
+              Price: <span className="text-gray-700">{price}</span>
+            </p>{" "}
+            <p className="text-gray-600">
+              Category:
+              <span className="text-gray-700">{category}</span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="pr-3 pt-2 flex flex-col gap-4">
+        <button className="  cursor-pointer ">
+          <SquarePen
+            size={27}
+            className="text-gray-500 hover:text-green-600 transition-colors duration-300"
+          />
+        </button>
+        <button className=" cursor-pointer  ">
+          <Trash2Icon
+            size={27}
+            className="text-gray-500 hover:text-red-600 transition-colors duration-300"
+          />
+        </button>
       </div>
     </div>
   );
@@ -30,6 +60,7 @@ const AdminProductsList = () => {
   return (
     <div className="py-5 px-2">
       <div>
+        <div></div>
         <input
           type="text"
           placeholder="Enter Product id"
