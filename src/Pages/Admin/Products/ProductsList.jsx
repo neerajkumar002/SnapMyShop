a7f500f9ac01a29a6f6fb397acf9865b8ad27e18
Neeaj Kumar <neerajkumar08697@gmail.com>
2025-04-03 10:09:25 +0530

    refactor: move AdminProductItem comoponent

--- src/Pages/Admin/Products/ProductsList.jsx
+++ src/Pages/Admin/Products/ProductsList.jsx
@@ -1,52 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAllProducts } from "../../../store/slice/product-slice";
-import { SquarePen, Trash2Icon } from "lucide-react";
-
-const AdminProductItem = ({ id, image, title, price, category }) => {
-  return (
-    <div className=" bg-white shadow-md flex justify-between gap-3 rounded-md ">
-      <div className="flex gap-3">
-        <div className="w-[80px]  overflow-hidden ">
-          <img
-            src={image || "/public/placeholder.svg"}
-            alt=""
-            className="w-full h-full "
-          />
-        </div>
-        <div>
-          <p className="text-gray-700">{title}</p>
-          <p className="text-gray-600">
-            Product ID: <span className="text-gray-700">{id}</span>{" "}
-          </p>
-          <div className="flex items-center gap-3">
-            <p className="text-gray-600">
-              Price: <span className="text-gray-700">{price}</span>
-            </p>{" "}
-            <p className="text-gray-600">
-              Category:
-              <span className="text-gray-700">{category}</span>
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="pr-3 pt-2 flex flex-col gap-4">
-        <button className="  cursor-pointer ">
-          <SquarePen
-            size={27}
-            className="text-gray-500 hover:text-green-600 transition-colors duration-300"
-          />
-        </button>
-        <button className=" cursor-pointer  ">
-          <Trash2Icon
-            size={27}
-            className="text-gray-500 hover:text-red-600 transition-colors duration-300"
-          />
-        </button>
-      </div>
-    </div>
-  );
-};
+import AdminProductItem from "../../../components/Admin/Products/ProductItem";
 
 const AdminProductsList = () => {
   const [searchProduct, setSearchProduct] = useState("");
