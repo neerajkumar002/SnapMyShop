ec1890f1a62a4834e65115b068534b0cc066d0d3
Neeaj Kumar <neerajkumar08697@gmail.com>
2025-04-20 11:38:40 +0530

    feat: add address management thunks for fetching, updating and deleting addresses

--- src/store/slice/Address-slice/index.js
+++ src/store/slice/Address-slice/index.js
@@ -4,6 +4,7 @@ import axios from "axios";
 const initialState = {
   addressList: [],
   isLoading: false,
+  addressData: null,
 };
 
 //add address
@@ -30,9 +31,59 @@ export const fetchAllAddresses = createAsyncThunk(
   async (userId) => {
     try {
       const response = await axios.get(
-        `${import.meta.env.VITE_BASE_API_URL}/address/${userId}`
+        `${import.meta.env.VITE_BASE_API_URL}/address/list/${userId}`
       );
 
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+//get address by address id
+export const getAddressById = createAsyncThunk(
+  "address/getAddressById",
+  async (id) => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_BASE_API_URL}/address/${id}`
+      );
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+//update address
+export const updateAddress = createAsyncThunk(
+  "address/updateAddress",
+  async ({ userId, addressId, formData }) => {
+    try {
+      const response = await axios.put(
+        `${
+          import.meta.env.VITE_BASE_API_URL
+        }/address/update/${userId}/${addressId}`,
+        formData
+      );
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+//delete address
+export const deleteAddress = createAsyncThunk(
+  "address/deleteAddress",
+  async ({ userId, addressId }) => {
+    try {
+      const response = await axios.delete(
+        `${
+          import.meta.env.VITE_BASE_API_URL
+        }/address/delete/${userId}/${addressId}`
+      );
+      console.log(response);
       return response.data;
     } catch (error) {
       console.log(error);
@@ -67,6 +118,29 @@ const addressSlice = createSlice({
       .addCase(fetchAllAddresses.rejected, (state) => {
         state.isLoading = true;
         state.addressList = [];
+      })
+      .addCase(getAddressById.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getAddressById.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.addressData = action.payload.data;
+      })
+      .addCase(getAddressById.rejected, (state) => {
+        state.isLoading = true;
+        state.addressData = [];
+      })
+      .addCase(updateAddress.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(updateAddress.fulfilled, (state, action) => {
+        state.isLoading = false;
+        console.log(action.payload);
+        state.addressData = action.payload.data;
+      })
+      .addCase(updateAddress.rejected, (state) => {
+        state.isLoading = true;
+        state.addressData = [];
       });
   },
 });
