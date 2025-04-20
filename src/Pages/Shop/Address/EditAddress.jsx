f0c666d5934089e0a26f6753de518d770a0d027f
Neeaj Kumar <neerajkumar08697@gmail.com>
2025-04-20 22:39:42 +0530

    add EditAddress page with address editing functionality

---
+++ src/Pages/Shop/Address/EditAddress.jsx
@@ -0,0 +1,10 @@
+import EditAddressForm from "../../../components/Shop/Address/EditAddressFrom";
+const EditAddress = () => {
+  return (
+    <div className="px-14">
+      <EditAddressForm />
+    </div>
+  );
+};
+
+export default EditAddress;
