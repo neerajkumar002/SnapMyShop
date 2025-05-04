ab9c0e3ff4ce2c8ca09a8ca99463e05c6d60096e
Neeaj Kumar <neerajkumar08697@gmail.com>
2025-05-04 23:01:59 +0530

    fix: add reviews reducer to store configuration

--- src/store/store.js
+++ src/store/store.js
@@ -4,6 +4,7 @@ import authReducer from "./slice/Auth-slice/index";
 import cartReducer from "./slice/Cart-Slice/index";
 import addressReducer from "./slice/Address-slice/index";
 import orderReducer from "./slice/Order-slice/index";
+import reviewsReducer from "./slice/Review-slice/index";
 import adminorderReducer from "./admin/order-slice/index";
 import adminProductReducer from "./admin/product-slice/index";
 const store = configureStore({
@@ -13,6 +14,7 @@ const store = configureStore({
     cart: cartReducer,
     address: addressReducer,
     order: orderReducer,
+    reviews: reviewsReducer,
     adminOrders: adminorderReducer,
     adminProduct: adminProductReducer,
   },
