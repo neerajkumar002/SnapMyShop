c43f66e70b43758ad6b80b5dc710023c8111c396
Neeaj Kumar <neerajkumar08697@gmail.com>
2025-05-04 23:01:12 +0530

    fix: enhance ProductDetail component with reviews functionality and UI improvements

--- src/Pages/Shop/Products/Detail.jsx
+++ src/Pages/Shop/Products/Detail.jsx
@@ -1,5 +1,5 @@
-import { Heart, Minus, Plus, Star, Truck } from "lucide-react";
-import React, { useEffect } from "react";
+import { Heart, Truck } from "lucide-react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getProductDetail } from "../../../store/slice/product-slice";
@@ -8,15 +8,19 @@ import ProductDetailsShimmer from "../../../components/Shop/Shimmer/ProductDetai
 import ReviewCard from "../../../components/Shop/Review/ReviewCard";
 import { addToCart } from "../../../store/slice/Cart-Slice";
 import { toast } from "react-toastify";
+import { fetchAllReviews } from "../../../store/slice/Review-slice";
+import ReviewModal from "../../../components/Shop/Review/ReviewModal";
+import StarRatingComponent from "../../../components/Shop/Review/StarRatingComponent";
 
 const ProductDetail = () => {
   const dispatch = useDispatch();
   const { id } = useParams();
 
-  const { productDetail, isLoading } = useSelector((state) => state.product);
   const { userData } = useSelector((state) => state.auth);
+  const { productDetail, isLoading } = useSelector((state) => state.product);
+  const { reviews } = useSelector((state) => state.reviews);
 
-  console.log(userData);
+  const [open, isOpen] = useState(false);
 
   useEffect(() => {
     if (id) {
@@ -24,7 +28,11 @@ const ProductDetail = () => {
     }
   }, [dispatch, id]);
 
-  if (isLoading) return <ProductDetailsShimmer />;
+  useEffect(() => {
+    if (id) {
+      dispatch(fetchAllReviews(id));
+    }
+  }, [dispatch, id]); 
 
   function handleAddToCart() {
     if (!userData) {
@@ -42,10 +50,16 @@ const ProductDetail = () => {
     }
   }
 
+  function closeModal() {
+    isOpen(false);
+  }
+ 
+
+  if (isLoading) return <ProductDetailsShimmer />;
+
   return (
     <div className="lg:px-6 ">
-      <div className="w-full  py-6  lg:px-8">Home > Mens </div>
-
+     
       <div className="flex flex-col  lg:flex-row gap-7 justify-center lg:px-8">
         <div className="lg:w-[500px]">
           <img src={productDetail?.image || "/public/placeholder.svg"} alt="" />
@@ -64,12 +78,8 @@ const ProductDetail = () => {
           <p className="text-xl text-red-600 font-semibold">
             ₹{productDetail?.price}
           </p>
-          <div className="flex gap-2">
-            <Star fill="gold" stroke="none" />
-            <Star fill="gold" stroke="none" />
-            <Star fill="gold" stroke="none" />
-            <Star fill="gold" stroke="none" />
-          </div>
+
+          <StarRatingComponent rating={productDetail?.averageReview} />
           <div className="flex items-center gap-3 py-5">
             <button
               onClick={() => handleAddToCart()}
@@ -82,15 +92,38 @@ const ProductDetail = () => {
             </button>
           </div>
           <div className="mb-3">
-            <h3 className="font-semibold text-xl py-2">Reviews</h3>
+            <div className="flex items-center justify-between">
+              <h3 className="font-semibold text-xl py-2">
+                Reviews ({reviews.length})
+              </h3>
+              {userData && (
+                <button
+                  onClick={() => isOpen(true)}
+                  className="bg-blue-400 text-white rounded-md px-3 py-1 hover:bg-blue-500 transition-colors duration-300 cursor-pointer"
+                >
+                  Add Review
+                </button>
+              )}
+            </div>
+            {open && (
+              <ReviewModal
+                closeModal={closeModal}
+                userId={userData?.id}
+                fullName={userData?.fullName}
+                productId={id}
+              />
+            )}
             <div className="flex flex-col gap-2 min-h-[100px] max-h-[240px] overflow-y-auto">
-              <ReviewCard />
-              <ReviewCard />
-              <ReviewCard />
-              <ReviewCard />
-              <ReviewCard />
-              <ReviewCard />
-              <ReviewCard />
+              {reviews &&
+                reviews.map((item) => (
+                  <ReviewCard
+                    key={item?._id}
+                    fullName={item?.fullName}
+                    reviewMessage={item?.reviewMessage}
+                    reviewValue={item?.reviewValue}
+                    dateTime={item?.createdAt}
+                  />
+                ))}
             </div>
           </div>
         </div>
