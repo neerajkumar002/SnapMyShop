bcae0e8a46b2b6d3b919287ef576d562d9bcef36
Neeaj Kumar <neerajkumar08697@gmail.com>
2025-05-04 22:53:16 +0530

    refactor: update ReviewCard to accept props for dynamic rendering of review details

--- src/components/Shop/Review/ReviewCard.jsx
+++ src/components/Shop/Review/ReviewCard.jsx
@@ -1,20 +1,19 @@
+import { format } from "date-fns";
 import { Star } from "lucide-react";
 
-const ReviewCard = () => {
+const ReviewCard = ({ fullName, reviewMessage, reviewValue, dateTime }) => {
   return (
     <div className="bg-white shadow-md px-2 py-2">
       <div className="flex justify-between">
         <div className="inline-flex items-center gap-3">
-          <p className="font-semibold text-xl">Neeraj Kumar</p>
+          <p className="font-semibold text-xl">{fullName}</p>
           <p className="inline-flex gap-2 bg-green-400 items-center px-2 rounded-full text-white font-semibold">
-            <Star color="gold " fill="gold" size={20} /> 5
+            <Star color="gold " fill="gold" size={20} /> {reviewValue}
           </p>
         </div>
-        <p>02/04/2025</p>
+        <p> {format(new Date(dateTime), "dd/MM/yyyy")}</p>
       </div>
-      <p className="text-sm lg:text-base">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, maiores.
-      </p>
+      <p className="text-sm lg:text-base">{reviewMessage}</p>
     </div>
   );
 };
