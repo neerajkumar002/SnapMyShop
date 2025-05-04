6e2a13727caea98312a3baa66de68f0472086e5a
Neeaj Kumar <neerajkumar08697@gmail.com>
2025-05-04 22:55:31 +0530

    feat: add Breadcrumbs component to ShopLayout for improved navigation

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Shop/Navbar/Navbar";
 import Footer from "../components/Shop/Footer/Footer";
+import Breadcrumbs from "../components/common/Breadcrumbs";
 const ShopLayout = () => {
   return (
     <div className="px-5 lg:px-20">
       <Navbar />
       <main className="min-h-screen">
+        <Breadcrumbs />
         <Outlet />
       </main>
       <Footer />
